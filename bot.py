f99fa5a2f9071c1ef0e9b60678a06bdc44606278
ZeyXs <47753293+ZeyXs@users.noreply.github.com>
2019-02-28 11:04:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,7 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='• Spectateurs')
-    await client.add_roles(member, role)
-       
+    await client.add_roles(member, role)      
     
 @client.event
 async def on_message(message):
